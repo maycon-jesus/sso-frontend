dc5b1b2d6b803eb2a6c4b1a4df78e0c5a422c6d0
Maycon Jesus <email@mayconjesus.dev>
2022-11-02 22:26:50 -0300

    feat: icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: true,
   css: [
     "vuetify/lib/styles/main.sass",
     "vue-toastification/dist/index.css",
+    "@mdi/font/css/materialdesignicons.css",
     "assets/scss/global.scss",
   ],
   build: {
