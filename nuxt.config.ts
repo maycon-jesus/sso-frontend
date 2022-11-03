b667c23ad773c7bc9154130bfeddc18228d4981b
Maycon Jesus <email@mayconjesus.dev>
2022-11-03 17:33:42 -0300

    refactor: css to cdn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,28 @@
 export default defineNuxtConfig({
   ssr: true,
   css: [
-    "vuetify/lib/styles/main.sass",
-    "vue-toastification/dist/index.css",
     "assets/scss/global.scss",
   ],
-  head:{
-    link:[
-      {
-        rel:'stylesheet',
-        href:'https://cdn.jsdelivr.net/npm/@mdi/font@6.9.96/css/materialdesignicons.min.css'
-      }
-    ]
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/vue-toastification@2.0.0-rc.5/dist/index.css',
+          crossorigin: ''
+        },
+        {
+          rel: "stylesheet",
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/vuetify/3.0.0/vuetify.css',
+          crossorigin: ''
+        },
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/@mdi/font@6.9.96/css/materialdesignicons.min.css",
+          crossorigin: "",
+        },
+      ],
+    },
   },
   build: {
     transpile: ["vuetify", "vue-toastification", "classnames"],
