f8d3dd593a82d68801ebe1e48f9b3eb2f8c0e8a1
Maycon Jesus <email@mayconjesus.dev>
2022-11-05 21:29:28 -0300

    perf: test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,47 +1,51 @@
+import vuetify from 'vite-plugin-vuetify'
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: true,
-  css: ["assets/scss/global.scss"],
+  css: ['assets/scss/global.scss'],
   app: {
     head: {
       link: [
         {
-          rel: "preconnect",
-          href: "https://cdnjs.cloudflare.com",
+          rel: 'preconnect',
+          href: 'https://cdnjs.cloudflare.com'
         },
         {
-          rel: "preconnect",
-          href: "https://cdn.jsdelivr.net",
+          rel: 'preconnect',
+          href: 'https://cdn.jsdelivr.net'
         },
         {
-          rel: "stylesheet",
-          href: "https://cdn.jsdelivr.net/npm/vue-toastification@2.0.0-rc.5/dist/index.css",
-          crossorigin: "",
-          media: "none",
-          onload: "if(media!='all')media='all'",
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/vue-toastification@2.0.0-rc.5/dist/index.css',
+          crossorigin: '',
+          media: 'none',
+          onload: "if(media!='all')media='all'"
         },
         {
-          rel: "stylesheet",
-          href: "https://cdnjs.cloudflare.com/ajax/libs/vuetify/3.0.0/vuetify.min.css",
-          crossorigin: ""
-        },
-        {
-          rel: "stylesheet",
-          href: "https://cdn.jsdelivr.net/npm/@mdi/font@6.9.96/css/materialdesignicons.min.css",
-          crossorigin: "",
-          media: "none",
-          onload: "if(media!='all')media='all'",
-        },
-      ],
-    },
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/@mdi/font@6.9.96/css/materialdesignicons.min.css',
+          crossorigin: '',
+          media: 'none',
+          onload: "if(media!='all')media='all'"
+        }
+      ]
+    }
   },
-  modules: ["@nuxt/image-edge"],
+  modules: [
+    '@nuxt/image-edge',
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        config.plugins.push(vuetify())
+      })
+    }
+  ],
   build: {
-    transpile: ["vuetify", "vue-toastification", "classnames"],
+    transpile: ['vuetify', 'vue-toastification', 'classnames']
   },
   runtimeConfig: {
     public: {
-      BASE_URL_API: process.env.NUXT_PUBLIC_BASE_URL_API,
-    },
-  },
-});
+      BASE_URL_API: process.env.NUXT_PUBLIC_BASE_URL_API
+    }
+  }
+})
