5837fd263c57a4f91eb1d6d3b05ed2cd879b1431
Maycon Jesus <email@mayconjesus.dev>
2022-11-19 13:07:44 -0300

    refactor: title bottom space

--- components/template/Title.vue
+++ components/template/Title.vue
@@ -1,9 +1,14 @@
 <template>
-  <v-row class="mt-2 mb-10">
+  <v-row class="mt-2 mb-5">
     <v-col cols="12">
       <h1 class="text-h2 text-center">
         {{ props.title }}
       </h1>
+    </v-col>
+    <v-col v-if="props.subtitle" cols="12">
+      <p class="text-h5 text-center">
+        {{ props.subtitle }}
+      </p>
     </v-col>
   </v-row>
 </template>
@@ -13,6 +18,11 @@ const props = defineProps({
   title: {
     type: String,
     required: true
+  },
+  subtitle: {
+    type: String,
+    required: false,
+    default: () => null
   }
 })
 </script>
