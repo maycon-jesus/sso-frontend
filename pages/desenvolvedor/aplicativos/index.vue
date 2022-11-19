57858a0757312ab9de96e0fb1e857dc9a90999b0
Maycon Jesus <email@mayconjesus.dev>
2022-11-19 13:23:25 -0300

    feat: init app page

---
+++ pages/desenvolvedor/aplicativos/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <template-title title="Aplicativos" subtitle="Aplicativos são um forma de conectar suas aplicações com o SSO" />
+
+    <v-row justify="end">
+      <v-col cols="auto">
+        <v-btn>
+          Criar aplicativo
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'user-area'
+})
+</script>
